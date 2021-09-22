2fab696ecee4a8900cde3e5caf85f47f4fe722ce
Design and Code <32168776+DACGitHub@users.noreply.github.com>
2021-09-22 11:11:39 +0100

    mobile menu and some bug fixing

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,71 @@
 
 	  <script type="text/javascript" src="js/load-more.js"></script>
    
+   
+   
+	  	<div class="menu-container bg-white xl:hidden" :class="fixedMenu ? 'menu-fixed' : ''">
+			<div class="container text-center text-darkblue text-center menu pt-4 pb-4">
+				<div class="grid grid-cols-2 lg:grid-cols-2 justify-items-end lg:justify-items-center items-center">
+
+					<div class="justify-self-start" style="width:100px;">
+						<a href="#home">
+						<img style="width:184px; max-width: none;" src="~assets/img/clarksons-bulk-cargo-mobile.svg">
+						</a>
+					</div>
+					<div class="burger-menu ml-auto" @click="menu = !menu">
+						<span class="burger-icon"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+			
+		<!-- Mobile nav list -->
+		<div v-show="menu" class="m-menu bg-darkgreen">
+			<div class="container text-white text-center pt-4 pb-4 menu">
+				<div class="grid grid-cols-2 lg:grid-cols-2 justify-items-end lg:justify-items-center items-center">
+					<div class="justify-self-start" style="width:100px;">
+						<a href="#home">
+						<img style="width:184px; max-width: none;" src="~assets/img/clarksons-bulk-cargo.svg">
+						</a>
+					</div>
+					<div class="burger-menu burger-close ml-auto xl:hidden" @click="menu = !menu">
+						<span class="burger-icon"></span>
+					</div>
+				</div>	
+				<div class="grid grid-cols-1 items-center mt-24"> 
+				<div class="m-menu-pages">
+					<nav>
+						<li class="mt-4 mb-4"><a class="footer-link menu-link" href="#cargo">Cargo</a></li>
+				  		<li class="mb-4"><a class="footer-link menu-link" href="#trader">Trader</a></li>
+				  		<li class="mb-4"><a class="footer-link menu-link" href="#broker">Broker</a></li>
+				  		<li class="mb-4"><a class="footer-link menu-link" href="#customs">Customs</a></li>
+				  		<li class="mb-4"><a class="footer-link menu-link" href="#ships-agent">Ships Agent</a></li>
+				  		<li class="mb-4"><a class="footer-link menu-link" href="#contact">Contact</a></li>
+			  		</nav>
+				</div>
+				</div>
+			</div>
+			<div class="fixed bottom-0 w-screen ">
+				<div class="mx-auto w-max">
+		  		<a target="_blank" href="https://uk.gofundme.com/f/single-handedly-navigating-the-world" class="button bg-green text-darkgreen w-full absolute left-0 bottom-0 p-4 rounded-tr-lg">Donate!</a>
+				</div>
+			</div>
+		</div>
+   
+   
+   
 
 		<header class="bg-darkblue">
 			<div class="container">
 
-				<div class="grid grid-cols-1 pt-16 pb-24">
+				<div class="grid grid-cols-1 pt-16 xl:pb-24 pb-12 hidden xl:block">
 					<div class="justify-self-start">
 						<img src="~assets/img/clarksons-bulk-cargo.svg">
 					</div>
 				</div>
 				<div class="grid grid-cols-1 justify-items-center items-center">
 					<div class="text-center pt-16 pb-48 xl:pb-72">
-						<h1 class="text-brightblue">Supporting growth across<br /> the Agri Bulk Sector</h1>
+						<h1 class="text-brightblue">Supporting growth across<br class="hidden xl:block" /> the Agri Bulk Sector</h1>
 					</div>
 				</div>
 
@@ -23,65 +76,66 @@
 		</header>
 
 		<!-- <section class="section-one flex h-screen justify-center items-center text-center"> -->
-		<section class="section-one text-center">
-			<img class="-mt-24 xl:-mt-48" src="~assets/img/first-section.svg">
+		<section class="section-one text-center" id="cargo">
+			<img class="-mt-16 xl:-mt-48" src="~assets/img/first-section.svg">
 			<div class="container pt-20 xl:pt-24 pb-20">
 				<h2 class="text-brightblue">Cargo</h2>
-				<p>Crops grown to meet<br /> demand in global market</p>
+				<p>Crops grown to meet<br class="hidden xl:block" /> demand in global market</p>
 			</div>
 			<img class="xl:-mt-32" src="~assets/img/second-section.svg">
 		</section>
-		<section class="section-two text-center">
+		<section class="section-two text-center" id="trader">
 			<div class="container pt-24 xl:pt-96">
 				<h2 class="text-brightblue">Trader</h2>
-				<p>Trader connects<br />crops to buyer</p>
+				<p>Trader connects<br class="hidden xl:block" /> crops to buyer</p>
 			</div>
 			<img class="hidden xl:block xl:-mt-80" src="~assets/img/third-section.svg">
 			<img class="block xl:hidden mt-12" src="~assets/img/third-section_mobile.svg">
 		</section>
-		<section class="section-three bg-lightblue text-center">
-			<img class="block xl:hidden mt-12 -ml-32 pt-32" src="~assets/img/fourth-section_mobile1.svg">
+		<section class="section-three bg-lightblue text-center" id="broker">
+			<img class="block xl:hidden xl:mt-12 -ml-32 pt-32" src="~assets/img/fourth-section_mobile1.svg">
 			<div class="container pt-16 xl:pt-48 pb-20">
 				<h2 class="text-brightblue">Broker</h2>
-				<p>Our Ship Brokers source the<br /> appropriate vessel and<br /> trading routes for shipment</p>
+				<p>Our Ship Brokers source the<br class="hidden xl:block" /> appropriate vessel and<br class="hidden xl:block"/> trading routes for shipment</p>
 			</div>
 			<img class="hidden xl:block xl:-mt-96" src="~assets/img/fourth-section.svg">
 			<img class="block xl:hidden" src="~assets/img/fourth-section_mobile2.svg">
 		</section>
-		<section class="section-three bg-lightblue text-center">
+		<section class="section-four bg-lightblue text-center" id="customs">
 			<div class="container pt-48 pb-20">
 				<h2 class="text-brightblue">Customs</h2>
-				<p>Our Customs Agents provide<br /> guidance and support to ensure<br /> the relevant customs clearance is<br /> completed in a timely manner</p>
+				<p>Our Customs Agents provide<br class="hidden xl:block" /> guidance and support to ensure<br class="hidden xl:block"/> the relevant customs clearance is<br class="hidden xl:block"/> completed in a timely manner</p>
 			</div>
 			<img class="hidden xl:block xl:-mt-80" src="~assets/img/fifth-section.svg">
 			<img class="block xl:hidden" src="~assets/img/fifth-section_mobile.svg">
 		</section>
-		<section class="section-three text-center">
+		<section class="section-five text-center" id="ships-agent">
 			<img class="block xl:hidden mb-16" src="~assets/img/sixth-section_mobile1.svg">
 			<div class="container pt-140 pb-20">
 				<h2 class="text-brightblue">Ships agent</h2>
-				<p>Our Ships Agents coordinate<br /> and facilitate all parties<br /> requirements within the port</p>
+				<p>Our Ships Agents coordinate<br class="hidden xl:block" /> and facilitate all parties<br class="hidden xl:block"/> requirements within the port</p>
 			</div>
 			<img class="hidden xl:block -mt-140 xl:-mb-32 xl:-ml-36" src="~assets/img/sixth-section.svg">
 			<img class="block xl:hidden -mb-20" src="~assets/img/sixth-section_mobile2.svg">
 		</section>
 		
-		<section class="section-three bg-lightblue text-center">
+		<section class="section-six bg-lightblue text-center">
 			<div class="container pt-32 xl:pt-48 pb-20">
-				<p>Our Ships Agents complete<br /> cargo documentation and<br /> arrange vessel’s departure</p>
+				<p>Our Ships Agents complete<br class="hidden xl:block" /> cargo documentation and<br class="hidden xl:block"/> arrange vessel’s departure</p>
 			</div>
 			<img class="xl:-mt-24" src="~assets/img/seventh-section.svg">
 <!-- 			<img class="block xl:hidden -ml-24" src="~assets/img/seventh-section_mobile.svg"> -->
 		</section>
 		
 		
-		<section class="pt-64 pb-64">
+		<section class="xl:pt-64 pt-16 xl:pb-64 pb-16" id="contact">
 			
 			<div class="container">
 				
+					<h2 class="h1 text-center">Contact us</h2>
 					<div class="grid lg:grid-cols-2">
 						<div class="">
-							<p>Select a location for<br /> contact information</p>
+							<p class="hidden xl:block">Select a location for<br /> contact information</p>
 						</div>
 						<div class="">
 						</div>
@@ -91,15 +145,15 @@
 			
 		</section>
 		
-		<section class="pt-64 pb-64">
+		<section class="xl:pt-64 xl:pb-64 pb-20">
 			
 			<div class="container">
 				
 				<p class="text-center">Bulk Cargo team</p>
 				
-					<div class="grid lg:grid-cols-3 mt-20 text-center">
-						<div>
-							<img class="m-auto mb-8" src="~assets/img/paul-hutchison@2x.png" style="width: auto;">
+					<div class="grid lg:grid-cols-3 xl:mt-20 mt-12 text-center">
+						<div class="mb-12 xl:mb-0">
+							<img class="m-auto mb-8" src="~assets/img/paul-hutchison@2x.png" style="width: 177px;">
 							<h3 class="mb-8"><span class="font-bold">Paul Hutchison</span><br />
 								Director</h3>
 							</h3>
@@ -107,22 +161,22 @@
 								<a href="mailto:paul.hutchinson@clarksons.com">paul.hutchinson@clarksons.com</a>
 							</p>
 						</div>
-						<div class="">
-							<img class="m-auto mb-8" src="~assets/img/steve-smith@2x.png" style="width: auto;">
-							<h3 class="mb-8"><span class="font-bold">Paul Hutchison</span><br />
+						<div class="mb-12 xl:mb-0">
+							<img class="m-auto mb-8" src="~assets/img/steve-smith@2x.png" style="width: 177px;">
+							<h3 class="mb-8"><span class="font-bold">Steve Smith</span><br />
 								Director</h3>
 							</h3>
-							<p class="small">+44 (0) 782 355 7090<br />
-								<a href="mailto:paul.hutchinson@clarksons.com">paul.hutchinson@clarksons.com</a>
+							<p class="small">+44 (0) 788 071 6071<br />
+								<a href="mailto:stephen.smith@clarksons.com">stephen.smith@clarksons.com</a>
 							</p>
 						</div>
 						<div class="">
-							<img class="m-auto mb-8" src="~assets/img/jennifer-leonard@2x.png" style="width: auto;">
-							<h3 class="mb-8"><span class="font-bold">Paul Hutchison</span><br />
-								Director</h3>
+							<img class="m-auto mb-8" src="~assets/img/jennifer-leonard@2x.png" style="width: 177px;">
+							<h3 class="mb-8"><span class="font-bold">Jennifer Leonard</span><br />
+								Business Development Manager</h3>
 							</h3>
-							<p class="small">+44 (0) 782 355 7090<br />
-								<a href="mailto:paul.hutchinson@clarksons.com">paul.hutchinson@clarksons.com</a>
+							<p class="small">+44 (0) 754 543 0152<br />
+								<a href="mailto:jennifer.leonard@clarksons.com">jennifer.leonard@clarksons.com</a>
 							</p>
 						</div>
 
@@ -134,36 +188,36 @@
 		
 		<section class="">
 			
-			<div class="container">
+			<div class="container pb-16">
 				
 				<p class="text-center">Shortsea Brokers</p>
 				
-					<div class="grid lg:grid-cols-3 mt-20 text-center">
-						<div class="">
-							<img class="m-auto mb-8" src="~assets/img/jennifer-leonard@2x.png" style="width: auto;">
-							<h3 class="mb-8"><span class="font-bold">Paul Hutchison</span><br />
-								Director</h3>
+					<div class="grid lg:grid-cols-3 xl:mt-20 mt-12 text-center">
+						<div class="mb-12 xl:mb-0">
+							<img class="m-auto mb-8" src="~assets/img/jennifer-leonard@2x.png" style="width: 177px;">
+							<h3 class="mb-8"><span class="font-bold">John Gardner</span><br />
+								Broker</h3>
 							</h3>
-							<p class="small">+44 (0) 782 355 7090<br />
-								<a href="mailto:paul.hutchinson@clarksons.com">paul.hutchinson@clarksons.com</a>
+							<p class="small">+44 (0) 782 766 7084<br />
+								<a href="mailto:shortsea@clarksons.com">shortsea@clarksons.com</a>
+							</p>
+						</div>
+						<div class="mb-12 xl:mb-0">
+							<img class="m-auto mb-8" src="~assets/img/mark-stent@2x.png" style="width: 177px;">
+							<h3 class="mb-8"><span class="font-bold">Mark Stent</span><br />
+								Broker</h3>
+							</h3>
+							<p class="small">+44 (0) 781 050 3919<br />
+								<a href="mailto:shortsea@clarksons.com">shortsea@clarksons.com</a>
 							</p>
 						</div>
 						<div class="">
-							<img class="m-auto mb-8" src="~assets/img/mark-stent@2x.png" style="width: auto;">
-							<h3 class="mb-8"><span class="font-bold">Paul Hutchison</span><br />
-								Director</h3>
+							<img class="m-auto mb-8" src="~assets/img/jennifer-leonard@2x.png" style="width: 177px;">
+							<h3 class="mb-8"><span class="font-bold">Lewis Gardner</span><br />
+								Broker</h3>
 							</h3>
-							<p class="small">+44 (0) 782 355 7090<br />
-								<a href="mailto:paul.hutchinson@clarksons.com">paul.hutchinson@clarksons.com</a>
-							</p>
-						</div>
-						<div class="">
-							<img class="m-auto mb-8" src="~assets/img/jennifer-leonard@2x.png" style="width: auto;">
-							<h3 class="mb-8"><span class="font-bold">Paul Hutchison</span><br />
-								Director</h3>
-							</h3>
-							<p class="small">+44 (0) 782 355 7090<br />
-								<a href="mailto:paul.hutchinson@clarksons.com">paul.hutchinson@clarksons.com</a>
+							<p class="small">+44 (0) 778 837 3479<br />
+								<a href="mailto:shortsea@clarksons.com">shortsea@clarksons.com</a>
 							</p>
 						</div>
 
@@ -186,7 +240,33 @@
 
 
 <script>
-
+export default {
+  components: {  },
+  data() {
+		return {
+			menu: false,
+			fixedMenu: false,
+		}
+  },
+	methods: {
+		handleScroll () {
+			if (window.scrollY > window.innerHeight + 0){
+				this.fixedMenu = true
+			} else {
+				this.fixedMenu = false
+			}
+		}
+	},
+	mounted () {
+		window.addEventListener('scroll', this.handleScroll);
+		window.scroll()
+	},
+	watch: {
+    '$route' () {
+      this.menu = false
+    }
+  }
+}
 </script>
 
 
