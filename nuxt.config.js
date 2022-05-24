535e5833b43ce035756cd149491562e807cb9add
Jack MB <jackmb@Jacks-iMac.local>
2022-05-24 10:11:03 +0100

    correcting social image

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Supporting growth across the Agri Bulk Sector' },
-      { hid: 'og:image', property: 'og:image', content: '/social-image.jpg' }
+      { hid: 'og:image', property: 'og:image', content: '/social-image.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
