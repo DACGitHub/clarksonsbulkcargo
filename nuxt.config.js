2896dcb44c0ef052206071a678a8997bb3bd6bb5
Jack MB <jackmb@Jacks-iMac.local>
2022-05-24 09:43:09 +0100

    added social image

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Supporting growth across the Agri Bulk Sector' },
+      { hid: 'og:image', property: 'og:image', content: '/social-image.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
